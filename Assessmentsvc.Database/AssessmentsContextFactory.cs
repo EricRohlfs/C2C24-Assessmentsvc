2444b3f28dab5767e85a3acd42fe7b81db661f53
sonofsysiphus <ricknice2000@gmail.com>
2018-03-01 19:10:46 -0500

    updated startup

--- Assessmentsvc.Database/AssessmentsContextFactory.cs
+++ Assessmentsvc.Database/AssessmentsContextFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.Extensions.Configuration;
+using Assessmentsvc.Database;
 using System.IO;
 
 namespace Assessmentsvc.Database
@@ -18,15 +19,25 @@ namespace Assessmentsvc.Database
 
 
             var builder = new DbContextOptionsBuilder<AssessmentsContext>().EnableSensitiveDataLogging(true);
+            if (configuration["USE_IN_MEMORY_DATABASE"] == "true")
+            {
+               
+                builder.UseInMemoryDatabase("Assessmentsvc.Database");
 
-            string dbname = configuration["MYSQL_DATABASE"];
-            string dbuser = configuration["MYSQL_USER"];
-            string dbpwd = configuration["MYSQL_PASSWORD"];
-            string dbserver = configuration["MYSQL_SERVICE_HOST"];
-            string dbport = configuration["MYSQL_SERVICE_PORT"];
-            var mySQLconnectionString = "Server=" + configuration["MYSQL_SERVICE_HOST"] + "; Port = " + configuration["MYSQL_SERVICE_PORT"] + "; Database = " + configuration["MYSQL_DATABASE"] + "; Uid= " + configuration["MYSQL_USER"] + ";Pwd=" + configuration["MYSQL_PASSWORD"] + ";";
+            }
+            else
+            {
 
-            builder.UseSqlServer(mySQLconnectionString);
+
+                string dbname = configuration["MYSQL_DATABASE"];
+                string dbuser = configuration["MYSQL_USER"];
+                string dbpwd = configuration["MYSQL_PASSWORD"];
+                string dbserver = configuration["MYSQL_SERVICE_HOST"];
+                string dbport = configuration["MYSQL_SERVICE_PORT"];
+                var mySQLconnectionString = "Server=" + configuration["MYSQL_SERVICE_HOST"] + "; Port = " + configuration["MYSQL_SERVICE_PORT"] + "; Database = " + configuration["MYSQL_DATABASE"] + "; Uid= " + configuration["MYSQL_USER"] + ";Pwd=" + configuration["MYSQL_PASSWORD"] + ";";
+
+                builder.UseMySql(mySQLconnectionString);
+            }
 
             return new AssessmentsContext(builder.Options);
         }
